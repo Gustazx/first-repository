66fa8f892e16ea78effb7ee0bbc2573641056369
Gustazx <120699453+Gustazx@users.noreply.github.com>
2023-03-22 14:47:57 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import ctypes
+
+
+def lock_screen():
+    ctypes.windll.user32.LockWorkStation()
+
+
+inputShut = input("Do you wanna lock your pc ? (yes/no): ")
+
+if inputShut == 'yes':
+    print("Locking your PC!")
+    lock_screen()
+else:
+    print("Ok!")
+    exit()
