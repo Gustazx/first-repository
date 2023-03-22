18a7eb3eae9c609e6f60f43f2bef83ee84064de1
your_username <gustavodelima841@gmail.com>
2023-03-22 17:29:59 -0300

    commiting

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import ctypes
+from time import sleep
+from win10toast_click import ToastNotifier
+
+notification = ToastNotifier()
+
+
+def showNotification():
+    notification.show_toast("Locking", "You choosed lock your PC", duration=3)
 
 
 def lock_screen():
+    '''locking screen function'''
     ctypes.windll.user32.LockWorkStation()
 
 
 inputShut = input("Do you wanna lock your pc ? (yes/no): ")
 
 if inputShut == 'yes':
-    print("Locking your PC!")
+    showNotification()
+    sleep(1)
     lock_screen()
 else:
     print("Leaving the program!")
